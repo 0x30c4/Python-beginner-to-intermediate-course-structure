776af3aa5722bc844d86ef5f614555f92fc3f98a
0x30c4 <abdullahsanaf@gmail.com>
2021-10-30 21:38:05 +0600

    Class 3

---
+++ Class_3#Loop/list.py
@@ -0,0 +1,10 @@
+# Basic List
+
+arr = [1, 2, 3, 4, "Hello"]
+#      0  1  2  3     4
+
+print(arr)
+print(arr[3])
+print(arr[1:])
+
+
---
+++ Class_3#Loop/task_1.py
@@ -0,0 +1,7 @@
+# In class task.
+# 1. Oparate a loop (For/While) on the given list (task_list)
+# 2. Then check if the number is odd or even.
+
+task_list = [234, 34, 13, 234, 25, 24, 623, 2367, 88, 99]
+
+
